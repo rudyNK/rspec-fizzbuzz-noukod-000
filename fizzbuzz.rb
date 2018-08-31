febe5d85f6789dd3c07373c38c572cea99378e15
Rudy Ravelin <rudy@noukod.com>
2018-08-31 19:05:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
     "Fizz" # Go fizz
   elsif int % 5 == 0 # if the number int is divisible by 5
       "Buzz" # Go Buzz
-    elsif int % 30 == 0 # if the number int is divisible by 15
+    elsif int % 15 == 0 # if the number int is divisible by 3 and 5
         "fizzbuzz" # Go FizzBuzz
   end
 end
